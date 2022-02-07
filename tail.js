39baa9caa07ea555bbcf300ab3f0462365d958ef
Pablo Tack <tackpablo@gmail.com>
2022-02-07 15:56:17 -0800

    head function

---
+++ tail.js
@@ -0,0 +1,17 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function (array) {
+  let tailValue = array.slice(1);
+  return tailValue;
+};
+
+// TEST CODE
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
