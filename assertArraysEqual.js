f1033d45d6d890c18f77b8c037baf5b3f3646f81
Pablo Tack <tackpablo@gmail.com>
2022-02-08 13:37:48 -0800

    assertArraysEqual.js logic

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function (actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const eqArrays = function (actual, expected) {
+  let eqArraysResult = eqArrays(actual, expected);
+
+  if (eqArraysResult === true) {
+    console.log(`✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
