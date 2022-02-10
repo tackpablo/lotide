dc9dec8c32d9b364320a6e3aa720555a255c4297
Pablo Tack <tackpablo@gmail.com>
2022-02-09 19:51:30 -0800

    eqObjects logic updated

--- eqObjects.js
+++ eqObjects.js
@@ -37,10 +37,15 @@ const eqObjects = function (object1, object2) {
 
   //loop through keys
   for (key of keys1) {
+    console.log(key);
     // check if values of both objects have an array (nested)
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      // if eqArrays (checking if both arrays are equal) returns is false, return false
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
       // return array check function to see if arrays are matching (nested arrays)
-      return eqArrays(object1[key], object2[key]);
+      // return eqArrays(object1[key], object2[key]);
     } else {
       // if any values do not match
       if (object1[key] !== object2[key]) {
@@ -52,16 +57,16 @@ const eqObjects = function (object1, object2) {
   return true;
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// assertEqual(eqObjects(ab, ba), true); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
+// const abc = { a: "1", b: "2", c: "3" };
+// assertEqual(eqObjects(ab, abc), false); // => false
 
 const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
+// const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true); // => true
 
-const cd2 = { c: "2", d: ["2", 3] };
+const cd2 = { c: "1", d: ["2", 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
