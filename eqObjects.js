5aea1451d16eb2472274e95be1c45bc433593bb3
Pablo Tack <tackpablo@gmail.com>
2022-02-09 18:56:49 -0800

    updated test for eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -54,14 +54,14 @@ const eqObjects = function (object1, object2) {
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba)); // => true
+assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc)); // => false
+assertEqual(eqObjects(ab, abc), false); // => false
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 assertEqual(eqObjects(cd, dc), true); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
+const cd2 = { c: "2", d: ["2", 3] };
 assertEqual(eqObjects(cd, cd2), false); // => false
