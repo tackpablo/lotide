848a769177e63e5da0edc9901537b3c91e66f60b
Pablo Tack <tackpablo@gmail.com>
2022-02-09 20:15:38 -0800

    assertObjectsEqual logic completed

--- eqObjects.js
+++ eqObjects.js
@@ -68,5 +68,5 @@ const cd = { c: "1", d: ["2", 3] };
 // const dc = { d: ["2", 3], c: "1" };
 // assertEqual(eqObjects(cd, dc), true); // => true
 
-const cd2 = { c: "1", d: ["2", 4] };
+const cd2 = { c: "2", d: ["2", 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
