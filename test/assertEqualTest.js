f9c3757d3d5ac155bee8828e55ed762b4cda6d23
Pablo Tack <tackpablo@gmail.com>
2022-02-13 21:12:04 -0800

    refactor assertEquals and head, separate tests

---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+// imports
+const assertEqual = require("../assertEqual");
+
+// TEST CODE
+
+// 1 assertEqual Test codes
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
